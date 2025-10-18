96e1ad5dafc44ca52020148d4a6cc0038b640f5f
RPC <ramisettipurnachandrikcd..>
2025-10-18 10:01:19 +0530

    commit

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendSpringbootappApplication {
+public class BackendSpringbootappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringbootappApplication.class, args);
---
+++ frontend-reactapp/src/config.js
@@ -0,0 +1,7 @@
+// config.js
+
+const config = {
+  API_URL: "http://localhost:2030/2300032803backend", // Backend URL
+};
+
+export default config;
